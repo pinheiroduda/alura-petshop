29f9e001c16081a61b77571bb92ef94422f480cb
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-20 14:48:07 -0300

    feat: adds switch to change between routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
@@ -10,12 +10,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/sobre">
-          <Sobre />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/sobre">
+            <Sobre />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </>
   )
