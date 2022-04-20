0ac1e3cf344c5cd01d249fd5575fb83e8fa2d1fc
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-20 14:45:29 -0300

    fix: returns the exact path depending on the route chosen

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
         </Route>
         <Route path="/sobre">
