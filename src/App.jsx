d4e10d4580c5bae5366f12346f1bcf3cbf20882c
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-20 14:12:13 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+
+import './assets/css/base/base.css'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Sobre />
+    </>
+  )
+}
+
+export default App
