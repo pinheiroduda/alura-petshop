2d6172da45313599534f056cfdeb6fe931a0bc66
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-09 11:04:07 -0300

    feat: get api subcategory posts

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams, Route, useRouteMatch } from 'react-router-dom'
 
 import ListaCategorias from '../components/ListaCategorias'
 import ListaPost from '../components/ListaPost'
 
 import '../assets/css/blog.css'
+import { busca } from '../api/api'
 
 const Categoria = () => {
   const { id } = useParams()
   const { path } = useRouteMatch()
+  const [subcategorias, setSubcategorias] = useState([])
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, categoria => {
+      setSubcategorias(categoria.subcategorias)
+    })
+  }, [id])
 
   return (
     <>
